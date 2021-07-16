3b34f51016697da2a7f0c0f23f57e16b200d6bf4
annin-tofu <yuok5770@gmail.com>
2021-07-17 01:04:40 +0900

    enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,7 @@
 import App from "./App";
+import Enzyme from "enzyme";
+import EnzymeAdapter from "@@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders learn react link", () => {});
