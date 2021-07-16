599143459289dd8ceb1a34572effb47ba72c7dac
annin-tofu <yuok5770@gmail.com>
2021-07-17 05:16:10 +0900

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ClickCounter from "./ClickCounter";
 
 function App() {
   return (
     <div className="App" id="" data-test="">
-      <h1>Hi I am a Click Counter App</h1>
+      <h1>Hi, I am Click Counter App</h1>
       <ClickCounter />
     </div>
   );
--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -14,7 +14,7 @@ function ClickCounter() {
   return (
     <div data-test="component-click-counter">
       <h1 data-test="counter-display">
-        The counter is currently<span data-test="count">{count}</span>
+        The counter is currently <span data-test="count">{count}</span>
       </h1>
       <button data-test="increment-button" onClick={incrementCounter}>
         Increment Counter
